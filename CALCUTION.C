85e3209c452836e4da6f14c58977ce79ee4c1514
PrasadKurhade <prasadkurhade128@gmail.com>
2022-10-28 17:14:14 +0530

    [ADDED]:Prasad CALCULATION file for user info

---
+++ CALCUTION.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+	int a=500,b=200,c=0;
+	c=a+b;
+	clrscr();
+	printf("\naddition=%d",c);
+	c=a-b;
+	printf("\nsubstaction=%d",c);
+	c=a*b;
+	printf("\nmultiplication=%d",c);
+	c=a/b;
+	printf("\ndivision=%d",c);
+	getch();
+}
